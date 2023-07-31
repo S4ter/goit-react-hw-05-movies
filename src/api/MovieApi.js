8e04f83dab817481e47c08eb201f09d701fbd5ed
Patryk Stefański <119447102+S4ter@users.noreply.github.com>
2023-07-31 20:15:34 +0200

    trending fetch

--- src/api/MovieApi.js
+++ src/api/MovieApi.js
@@ -1,6 +1,6 @@
 const API_KEY = 'f7559877f1d0830f5dd1b2777f8228a1';
 
-export const fetchImages = async inputSearch => {
+export const fetchImages = async () => {
   try {
     const response = await fetch(
       `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`
@@ -11,7 +11,6 @@ export const fetchImages = async inputSearch => {
     }
 
     const data = await response.json();
-    console.log(data.results);
     return data.results;
   } catch (error) {
     console.log(error);
--- src/components/Trending.jsx
+++ src/components/Trending.jsx
@@ -1,7 +1,26 @@
+import React, { useEffect, useState } from 'react';
+import { fetchImages } from 'api/MovieApi';
+
 export const Trending = () => {
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    const fetchTrendingMovies = async () => {
+      try {
+        const results = await fetchImages();
+        setMovies(results);
+      } catch (error) {
+        console.log('Error', error);
+      }
+    };
+    fetchTrendingMovies();
+  }, []);
+  console.log(movies);
   return (
-    <div>
-      <p>Trending list</p>
-    </div>
+    <ul className="trendingList">
+      {movies.map(el => (
+        <li key={el.id}>{el.title}</li>
+      ))}
+    </ul>
   );
 };
